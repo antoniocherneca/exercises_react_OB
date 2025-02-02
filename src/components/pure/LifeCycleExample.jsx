13b698ccb5b6dd4895733eda10b4f5515dc3c43e
antoniocherneca <antumaster09@gmail.com>
2025-02-02 03:32:21 -0300

    comenzando con ciclo de vida de componentes de clase

---
+++ src/components/pure/LifeCycleExample.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class LifeCycleExample extends Component {
+  constructor(props) {
+    super(props);
+    console.log("componente instanciado");
+  }
+
+  componentWillMount() {
+    console.log("Antes de montar el componente");
+  }
+
+  componentDidMount() {
+    console.log("El componente está listo pero todavía no ha sido pintado");
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("Si va a recibir nuevas props");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // Controlar si el componente debe actualizarse o no
+    // Debe devolver true || false
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log("Justo antes de actualizar el componente");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Se actualizó el componente");
+  }
+
+  componentWillUnmount() {
+    console.log("Justo antes de desmontar el componente");
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+LifeCycleExample.propTypes = {};
+
+export default LifeCycleExample;
